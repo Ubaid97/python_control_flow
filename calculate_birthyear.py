deee5f164bf6cb998351922513773b8605c7d4ee
Ubaid-ur-rehman Muhammad <zdac091@live.rhul.ac.uk>
2020-11-03 22:13:38 +0000

    added code comments

--- calculate_birthyear.py
+++ calculate_birthyear.py
@@ -1,15 +1,17 @@
+# Calculates birth year by taking age and subtracting it from the current year (2020)
 name = "John Doe"
 age = 22
 birth_year = 2020 - age
 print(f"OMG {name}, you're {age} years old and so were born in {birth_year}")
 
+# User asked to input name, age, the day of the month, and the month in which they born
 name = input("Enter your full name: ")
 age = int(input("Enter your age: "))
 birth_date = int(input("Please enter the day of the month you were born in: "))
 birth_month = input("What month were you born in: ").capitalize()
+# input for birth month is capitalised to ensure the == function in the if statement below functions
 
-months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-
+# if statement checking whether the birth date (day/month) given by the user is after the 3rd of November or before. if after, user told that their birthday has yet to happen this year. if before, they're told that it's already passed
 if (birth_month == "November" and birth_date > 3) or (birth_month == "December"):
     print("your birthday hasn't happened yet this year")
 else:
--- loops_exercise.py
+++ loops_exercise.py
@@ -1,24 +1,23 @@
 # Loop with range that says hello 10 times
+for i in range(10):
+    print("Hello")
 
-# for i in range(10):
-#     print("Hello")
-#
+# for loop that iterates over a range of 5 and adds a name (input by user) to a list in each iteration
 list_names = []
 for i in range(5):
     list_names.append(input("enter a name: "))
 
 print(list_names)
 
-
-# list_names_lower = []
-# for items in list_names:
-#     list_names_lower.append(items.lower())
-#
-# print(list_names_lower)
-
-
+# for loop that iterates over a list of names and converts all of them to lower case
+list_names_lower = []
 for items in list_names:
+    list_names_lower.append(items.lower())
 
+print(list_names_lower)
+
+# for loop that iterates over a list of names, and displays whether the name has an even or odd number of characters using an if statement
+for items in list_names:
     if len(items)%2 == 0:
         print(f"{items} has an even number of letters")
     else:
