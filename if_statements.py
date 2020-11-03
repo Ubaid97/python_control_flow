c31585ccbec15a9851c8ae713115c165c2b2ae7d
Ubaid-ur-rehman Muhammad <zdac091@live.rhul.ac.uk>
2020-11-03 15:38:48 +0000

    control flow repo

---
+++ if_statements.py
@@ -0,0 +1,9 @@
+
+age = int(input("What is your age: "))
+
+if age > 15:
+    print("You're old enough to watch this movie")
+elif age <= 15:
+    print("Sorry, you're not old enough to watch this movie. Please try another one")
+else:
+    print("oops, something went wrong. Try again")
