363d72fe8b85be5bf51b887b2a21af257414cb1d
Ubaid-ur-rehman Muhammad <zdac091@live.rhul.ac.uk>
2020-11-03 17:51:26 +0000

    last task of loops exercise

--- loops_exercise.py
+++ loops_exercise.py
@@ -10,8 +10,16 @@ for i in range(5):
 print(list_names)
 
 
-list_names_lower = []
-for items in list_names:
-    list_names_lower.append(items.lower())
+# list_names_lower = []
+# for items in list_names:
+#     list_names_lower.append(items.lower())
+#
+# print(list_names_lower)
 
-print(list_names_lower)
+
+for items in list_names:
+
+    if len(items)%2 == 0:
+        print(f"{items} has an even number of letters")
+    else:
+        print(f"{items} has an odd number of letters")
